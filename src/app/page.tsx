6c0c67d86c9b2e43195d7b4ff21a9f4de48ef77a
Connor <hu@lipalater.com>
2024-05-15 17:31:51 +0800

    feat: finished the database configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import FormTodo from "@/app/form/FormTodo";
 import { getList } from "@/server/posts";
 import HomePage from ".";
 
@@ -6,7 +5,6 @@ export default async function Home() {
   let dataList: any = await getList();
   return (
     <main className="">
-      {/* <FormTodo data={dataList}></FormTodo> */}
       <HomePage data={dataList}></HomePage>
     </main>
   );
